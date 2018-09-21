2d30892756af5082265748636fb3fb5c03e50927
Ngô Ngọc Quí <tranphanchau10@gmail.com>
2018-09-21 15:31:30 +0700

    updata api limit collection
     // Lấy tất cả collection giới hạn số lượng
        app.get('/collection/limit/:limit', collection.findAllLimit);

--- app/controllers/collection.controller.js
+++ app/controllers/collection.controller.js
@@ -82,7 +82,31 @@ exports.findAll = (req, res) => {
 // Lấy tất cả collection giới hạn số lượng
 exports.findAllLimit = (req, res) => {
     let limit = Number(req.params.limit);
-    Collection.find({}, { "videos.urlReal": 0, "videos.otherLink.urlReal": 0 })
+    Collection.find({}, { 
+        "videos.urlReal": 0, 
+        "videos.otherLink.urlReal": 0,
+        part: 0,
+        episodesCurrent: 0,
+        content: 0,
+        contentImages: 0,
+        releaseYear: 0,
+        categories: 0,
+        group: 0,
+        indexGroup: 0,
+        producer: 0,
+        fansub: 0,
+        followers: 0,
+        filmActor: 0,
+        filmDirector: 0,
+        page: 0,
+        status: 0,
+        countries: 0,
+        author: 0,
+        linkTrailer: 0,
+        startMusicName: 0,
+        finishMusicName: 0,
+        videos: 0
+    })
     .limit(limit)
     .then(result => {
        res.send(result);
